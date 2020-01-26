625b77906a4902c25887089ad1e2a94c3233ab29
OzkrMonroy <44916160+OzkrMonroy@users.noreply.github.com>
2020-01-26 13:23:31 -0600

    Updates

---
+++ precache-manifest.1abecbb8531ec4bc418b4f5c10ccb831.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808bbe2ba2638022d05d4b326a60bf5b",
+    "url": "/luxury-development/index.html"
+  },
+  {
+    "revision": "75945d43ea6fed126b81",
+    "url": "/luxury-development/static/css/main.d2d8451d.chunk.css"
+  },
+  {
+    "revision": "9186c2a82a32f4a0e186",
+    "url": "/luxury-development/static/js/2.19beddf2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/luxury-development/static/js/2.19beddf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75945d43ea6fed126b81",
+    "url": "/luxury-development/static/js/main.d013b534.chunk.js"
+  },
+  {
+    "revision": "52379814285693168cf3",
+    "url": "/luxury-development/static/js/runtime-main.36fe52a1.js"
+  }
+]);
